f58fa0361e26c4cf231538001a86a10986fbe9b7
Ridarhm09 <rida.rahmaniah@mhs.itenas.ac.id>
2026-07-15 08:19:11 +0700

    second

---
+++ Assets/Scripts/TestingBluetooth.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TestingBluetooth : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
